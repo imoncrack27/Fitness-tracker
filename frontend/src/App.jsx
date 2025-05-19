32d7976b1cb7e515c8bd9bf6ce87dc8bb335606e
imoncrack27 <mingodrexell321@gmail.com>
2025-05-19 23:25:41 +0800

    added protection routes for dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/register" element={<Register />} />
 
         <Route path="/login" element={<Login />} />
