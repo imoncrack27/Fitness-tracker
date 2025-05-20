081d423a6a4a6c7dd5c4b795ee6c27680836c7b8
imoncrack27 <mingodrexell321@gmail.com>
2025-05-20 19:26:28 +0800

    navbar cant be seen on login and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,13 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route
           path="/"
           element={
             <ProtectedRoute>
+              {" "}
+              <Navbar />
               <Dashboard />
             </ProtectedRoute>
           }
